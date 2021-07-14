7d449fe2cf59be0c1e4588a12405ec9be3df0d04
Isaac L. Muñoz <mcisaacleonardo@gmail.com>
2021-07-13 20:05:49 -0400

    adding features

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const $pause = document.querySelector('#pause')
 const $forward = document.querySelector('#forward')
 const $backward = document.querySelector('#backward')
 const $progress = document.querySelector('#progress')
+const $selector = document.querySelector('.selector')
+const $bar = document.querySelector('.progress-bar')
 
 $play.addEventListener('click', () => {
   $video.play()
@@ -33,8 +35,37 @@ $video.addEventListener('loadedmetadata', () => {
 
 $progress.addEventListener('input', () => {
   $video.currentTime = $progress.value
+  // Mover el selector
+  let currentPercentage = (100 * $video.currentTime) / $video.duration
+  $selector.style.left = currentPercentage + '%'
+  // Actualizar el tiempo en el selector
+  updateTime()
+  // Actualizar barra de progreso
+  updateBar()
 })
 
 $video.addEventListener('timeupdate', () => {
   $progress.value = $video.currentTime
+  // Mover el selector
+  let currentPercentage = (100 * $video.currentTime) / $video.duration
+  $selector.style.left = currentPercentage + '%'
+  // Actualizar el tiempo en el selector
+  updateTime()
+  // Actualizar barra de progreso
+  updateBar()
 })
+
+function updateTime() {
+  let firstNum  = $progress.value < 10
+                ? 0
+                : Math.floor($progress.value / 10)
+  let lastNum = $progress.value < 10
+              ? Math.floor($progress.value)
+              : Math.floor($progress.value - 10 * firstNum)
+  $selector.innerHTML = `0:${firstNum}${lastNum}`
+}
+
+function updateBar() {
+  let currentPercentage = (100 * $video.currentTime) / $video.duration
+  $bar.style['inline-size'] = currentPercentage + '%'
+}
